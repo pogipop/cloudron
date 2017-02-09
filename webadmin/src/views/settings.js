ca2d5957e422778361cf37155219e9767fc93123
Girish Ramakrishnan <girish@cloudron.io>
2017-02-09 12:58:54 -0800

    Add canada and london to backup regions
    Fixes #208

--- webadmin/src/views/settings.js
+++ webadmin/src/views/settings.js
@@ -33,18 +33,20 @@ angular.module('Application').controller('SettingsController', ['$scope', '$loca
 
     // List is from http://docs.aws.amazon.com/general/latest/gr/rande.html#s3_region
     $scope.s3Regions = [
-        { name: 'US East (N. Virginia)', value: 'us-east-1' },
-        { name: 'US East (Ohio)', value: 'us-east-2' },
-        { name: 'US West (N. California)', value: 'us-west-1' },
-        { name: 'US West (Oregon)', value: 'us-west-2' },
         { name: 'Asia Pacific (Mumbai)', value: 'ap-south-1' },
         { name: 'Asia Pacific (Seoul)', value: 'ap-northeast-2' },
         { name: 'Asia Pacific (Singapore)', value: 'ap-southeast-1' },
         { name: 'Asia Pacific (Sydney)', value: 'ap-southeast-2' },
         { name: 'Asia Pacific (Tokyo)', value: 'ap-northeast-1' },
+        { name: 'Canada (Central)', value: 'ca-central-1' },
         { name: 'EU (Frankfurt)', value: 'eu-central-1' },
         { name: 'EU (Ireland)', value: 'eu-west-1' },
-        { name: 'South America (São Paulo)', value: 'sa-east-1' }
+        { name: 'EU (London)', value: 'eu-west-2' },
+        { name: 'South America (São Paulo)', value: 'sa-east-1' },
+        { name: 'US East (N. Virginia)', value: 'us-east-1' },
+        { name: 'US East (Ohio)', value: 'us-east-2' },
+        { name: 'US West (N. California)', value: 'us-west-1' },
+        { name: 'US West (Oregon)', value: 'us-west-2' },
     ];
 
     $scope.planChange = {
