b0037b6141bd1d4a9363fd3489bb951e066e37bc
Girish Ramakrishnan <girish@cloudron.io>
2017-02-14 12:23:17 -0800

    Update infra images to use latest base image

--- src/infra_version.js
+++ src/infra_version.js
@@ -14,11 +14,11 @@ exports = module.exports = {
     // This is because we upgrade using dumps instead of mysql_upgrade, pg_upgrade etc
     'images': {
         'mysql': { repo: 'cloudron/mysql', tag: 'cloudron/mysql:0.14.0' },
-        'postgresql': { repo: 'cloudron/postgresql', tag: 'cloudron/postgresql:0.15.0' },
-        'mongodb': { repo: 'cloudron/mongodb', tag: 'cloudron/mongodb:0.11.0' },
-        'redis': { repo: 'cloudron/redis', tag: 'cloudron/redis:0.10.0' },
-        'mail': { repo: 'cloudron/mail', tag: 'cloudron/mail:0.29.0' },
-        'graphite': { repo: 'cloudron/graphite', tag: 'cloudron/graphite:0.10.0' }
+        'postgresql': { repo: 'cloudron/postgresql', tag: 'cloudron/postgresql:0.16.0' },
+        'mongodb': { repo: 'cloudron/mongodb', tag: 'cloudron/mongodb:0.12.0' },
+        'redis': { repo: 'cloudron/redis', tag: 'cloudron/redis:0.11.0' },
+        'mail': { repo: 'cloudron/mail', tag: 'cloudron/mail:0.30.0' },
+        'graphite': { repo: 'cloudron/graphite', tag: 'cloudron/graphite:0.11.0' }
     }
 };
 
