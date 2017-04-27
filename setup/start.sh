6cbf64b88e31811b97eb3bac99d3041a3a823c07
Girish Ramakrishnan <girish@cloudron.io>
2017-04-27 09:40:11 -0700

    use openssl password only when restore key is non-empty or backup ends with .enc

--- setup/start.sh
+++ setup/start.sh
@@ -208,10 +208,16 @@ mysql -u root -p${mysql_root_password} -e 'CREATE DATABASE IF NOT EXISTS box'
 if [[ -n "${arg_restore_url}" ]]; then
     set_progress "30" "Downloading restore data"
 
-    echo "==> Downloading backup: ${arg_restore_url} and key: ${arg_restore_key}"
+    openssl_password=""
+    if [[ "${arg_restore_url}" == *.tar.gz.enc || -n "${arg_restore_key}" ]]; then
+        echo "==> Downloading encrypted backup: ${arg_restore_url} and key: ${arg_restore_key}"
+        openssl_password="-nosalt -pass 'pass:${arg_restore_key}'"
+    else
+        echo "==> Downloading backup: ${arg_restore_url}"
+    fi
 
     while true; do
-        if $curl -L "${arg_restore_url}" | openssl aes-256-cbc -d -nosalt -pass "pass:${arg_restore_key}" \
+        if $curl -L "${arg_restore_url}" | openssl aes-256-cbc -d "${openssl_password}" \
         | tar -zxf - --overwrite --transform="s,^box/\?,boxdata/," --transform="s,^mail/\?,platformdata/mail/," --show-transformed-names -C "${HOME_DIR}"; then break; fi
         echo "Failed to download data, trying again"
     done
