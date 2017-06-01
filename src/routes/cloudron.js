4e0bb9187ae32abdc754a2f43916ca8cc07ecccc
Girish Ramakrishnan <girish@cloudron.io>
2017-06-01 09:26:03 -0700

    lower case domain in migrate code path

--- src/routes/cloudron.js
+++ src/routes/cloudron.js
@@ -164,6 +164,8 @@ function migrate(req, res, next) {
     var options = _.pick(req.body, 'domain', 'size', 'region');
     if (Object.keys(options).length === 0) return next(new HttpError(400, 'no migrate option provided'));
 
+    if (options.domain) options.domain = options.domain.toLowerCase();
+
     cloudron.migrate(req.body, function (error) { // pass req.body because 'domain' can have arbitrary options
         if (error && error.reason === CloudronError.BAD_STATE) return next(new HttpError(409, error.message));
         if (error && error.reason === CloudronError.BAD_FIELD) return next(new HttpError(400, error.message));
