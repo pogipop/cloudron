59c3525bc2f2a89e31794db919f83c76784b56ae
Girish Ramakrishnan <girish@cloudron.io>
2017-06-21 22:17:32 -0700

    move getSubscription to appstore.js

--- src/appstore.js
+++ src/appstore.js
@@ -4,6 +4,8 @@ exports = module.exports = {
     purchase: purchase,
     unpurchase: unpurchase,
 
+    getSubscription: getSubscription,
+
     sendAliveStatus: sendAliveStatus,
 
     getAppUpdate: getAppUpdate,
@@ -69,6 +71,25 @@ function getAppstoreConfig(callback) {
     }
 }
 
+function getSubscription(callback) {
+    assert.strictEqual(typeof callback, 'function');
+
+    getAppstoreConfig(function (error, appstoreConfig) {
+        if (error) return callback(error);
+
+        const url = config.apiServerOrigin() + '/api/v1/users/' + appstoreConfig.userId + '/cloudrons/' + appstoreConfig.cloudronId + '/subscription';
+        superagent.get(url).query({ accessToken: appstoreConfig.token }).timeout(30 * 1000).end(function (error, result) {
+            if (error && !error.response) return callback(new AppstoreError(AppstoreError.EXTERNAL_ERROR, error.message));
+            if (result.statusCode === 401) return callback(new AppstoreError(AppstoreError.EXTERNAL_ERROR, 'invalid appstore token'));
+            if (result.statusCode === 403) return callback(new AppstoreError(AppstoreError.EXTERNAL_ERROR, 'wrong user'));
+            if (result.statusCode === 502) return callback(new AppstoreError(AppstoreError.EXTERNAL_ERROR, 'stripe error'));
+            if (result.statusCode !== 200) return callback(new AppstoreError(AppstoreError.EXTERNAL_ERROR, 'unknown error'));
+
+            callback(null, result.body.subscription);
+        });
+    });
+}
+
 function purchase(appId, appstoreId, callback) {
     assert.strictEqual(typeof appId, 'string');
     assert.strictEqual(typeof appstoreId, 'string');
@@ -83,7 +104,7 @@ function purchase(appId, appstoreId, callback) {
         var data = { appstoreId: appstoreId };
 
         superagent.post(url).send(data).query({ accessToken: appstoreConfig.token }).timeout(30 * 1000).end(function (error, result) {
-            if (error && !error.response) return callback(new AppstoreError(AppstoreError.EXTERNAL_ERROR, error));
+            if (error && !error.response) return callback(new AppstoreError(AppstoreError.EXTERNAL_ERROR, error.message));
             if (result.statusCode === 404) return callback(new AppstoreError(AppstoreError.NOT_FOUND));
             if (result.statusCode === 403 || result.statusCode === 401) return callback(new AppstoreError(AppstoreError.BILLING_REQUIRED));
             if (result.statusCode !== 201 && result.statusCode !== 200) return callback(new AppstoreError(AppstoreError.EXTERNAL_ERROR, util.format('App purchase failed. %s %j', result.status, result.body)));
