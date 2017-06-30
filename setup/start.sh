50e712a93ec0c838a37375e99ea9a5becb4f0967
Girish Ramakrishnan <girish@cloudron.io>
2017-06-30 16:50:31 -0500

    preserve existing docker storage driver
    fixes #364

--- setup/start.sh
+++ setup/start.sh
@@ -40,9 +40,13 @@ systemctl enable apparmor
 systemctl restart apparmor
 
 usermod ${USER} -a -G docker
+# preserve the existing storage driver (user might be using overlay2)
+storage_driver=$(docker info | grep "Storage Driver" | sed 's/.*: //')
+[[ -n "${storage_driver}" ]] || storage_driver="devicemapper" # if the above command fails
+
 temp_file=$(mktemp)
 # create systemd drop-in. some apps do not work with aufs
-echo -e "[Service]\nExecStart=\nExecStart=/usr/bin/dockerd -H fd:// --log-driver=journald --exec-opt native.cgroupdriver=cgroupfs --storage-driver=devicemapper" > "${temp_file}"
+echo -e "[Service]\nExecStart=\nExecStart=/usr/bin/dockerd -H fd:// --log-driver=journald --exec-opt native.cgroupdriver=cgroupfs --storage-driver=${storage_driver}" > "${temp_file}"
 
 systemctl enable docker
 # restart docker if options changed
