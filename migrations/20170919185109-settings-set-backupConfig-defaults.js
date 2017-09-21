21c534c806fb0cb463570d7d357f90737becdb45
Girish Ramakrishnan <girish@cloudron.io>
2017-09-21 09:49:10 -0700

    Ensure format is set in backupConfig

--- migrations/20170919185109-settings-set-backupConfig-defaults.js
+++ migrations/20170919185109-settings-set-backupConfig-defaults.js
@@ -1,14 +1,16 @@
 'use strict';
 
-// ensure backupFolder has not empty
+// ensure backupFolder and format are not empty
 exports.up = function(db, callback) {
     db.all('SELECT * FROM settings WHERE name=?', [ 'backup_config' ], function (error, result) {
         if (error || result.length === 0) return callback(error);
 
         var value = JSON.parse(result[0].value);
-        if (value.provider !== 'filesystem' || value.backupFolder) return callback();
+        value.format = 'tgz'; // set the format
 
-        value.backupFolder = '/var/backups';
+        if (value.provider === 'filesystem' && !value.backupFolder) {
+            value.backupFolder = '/var/backups'; // set the backupFolder
+        }
 
         db.runSql('UPDATE settings SET value = ? WHERE name = ?', [ JSON.stringify(value), 'backup_config' ], function (error) {
             if (error) console.error('Error setting ownerid ' + JSON.stringify(u) + error);
