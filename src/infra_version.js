aa8ce2c62e8df65d55f59d1ad40bc1e0aef9856d
Girish Ramakrishnan <girish@cloudron.io>
2017-09-27 15:35:53 -0700

    Use graphite 0.12.0
    this fixes an issue where carbon does not startup properly
    if a previous pid file was present

--- src/infra_version.js
+++ src/infra_version.js
@@ -19,6 +19,6 @@ exports = module.exports = {
         'mongodb': { repo: 'cloudron/mongodb', tag: 'cloudron/mongodb:0.13.0' },
         'redis': { repo: 'cloudron/redis', tag: 'cloudron/redis:0.11.0' },
         'mail': { repo: 'cloudron/mail', tag: 'cloudron/mail:0.37.0' },
-        'graphite': { repo: 'cloudron/graphite', tag: 'cloudron/graphite:0.11.0' }
+        'graphite': { repo: 'cloudron/graphite', tag: 'cloudron/graphite:0.12.0' }
     }
 };
