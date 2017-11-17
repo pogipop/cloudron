6558c78094ff7fa6330514539e019c89b83a77c7
Girish Ramakrishnan <girish@cloudron.io>
2017-11-17 15:52:40 -0800

    change the json blobs to text

--- migrations/20171116203507-apps-rename-newConfigJson-to-updateConfigJson.js
+++ migrations/20171116203507-apps-rename-newConfigJson-to-updateConfigJson.js
@@ -1,14 +1,14 @@
 'use strict';
 
 exports.up = function(db, callback) {
-    db.runSql('ALTER TABLE apps CHANGE newConfigJson updateConfigJson VARCHAR(2048)', [], function (error) {
+    db.runSql('ALTER TABLE apps CHANGE newConfigJson updateConfigJson TEXT', [], function (error) {
         if (error) console.error(error);
         callback(error);
     });
 };
 
 exports.down = function(db, callback) {
-    db.runSql('ALTER TABLE apps CHANGE updateConfigJson newConfigJson VARCHAR(2048)', [], function (error) {
+    db.runSql('ALTER TABLE apps CHANGE updateConfigJson newConfigJson TEXT', [], function (error) {
         if (error) console.error(error);
         callback(error);
     });
--- migrations/20171116224051-apps-rename-lastBackupId-to-restoreConfigJson.js
+++ migrations/20171116224051-apps-rename-lastBackupId-to-restoreConfigJson.js
@@ -1,14 +1,14 @@
 'use strict';
 
 exports.up = function(db, callback) {
-    db.runSql('ALTER TABLE apps CHANGE lastBackupId restoreConfigJson VARCHAR(256)', [], function (error) {
+    db.runSql('ALTER TABLE apps CHANGE lastBackupId restoreConfigJson TEXT', [], function (error) {
         if (error) console.error(error);
         callback(error);
     });
 };
 
 exports.down = function(db, callback) {
-    db.runSql('ALTER TABLE apps CHANGE restoreConfigJson lastBackupId VARCHAR(256)', [], function (error) {
+    db.runSql('ALTER TABLE apps CHANGE restoreConfigJson lastBackupId TEXT', [], function (error) {
         if (error) console.error(error);
         callback(error);
     });
