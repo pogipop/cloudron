8dcb06cb02ca207345047d34bb1c0b92bc864c08
Johannes Zellner <johannes@cloudron.io>
2017-11-17 23:41:22 +0100

    Fix db migration down step for newConfigJson change

--- migrations/20171116203507-apps-rename-newConfigJson-to-updateConfigJson.js
+++ migrations/20171116203507-apps-rename-newConfigJson-to-updateConfigJson.js
@@ -8,7 +8,7 @@ exports.up = function(db, callback) {
 };
 
 exports.down = function(db, callback) {
-    db.runSql('ALTER TABLE apps CHANGE newConfigJson updateConfigJson VARCHAR(2048)', [], function (error) {
+    db.runSql('ALTER TABLE apps CHANGE updateConfigJson newConfigJson VARCHAR(2048)', [], function (error) {
         if (error) console.error(error);
         callback(error);
     });
