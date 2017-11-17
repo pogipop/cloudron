b84f60671ed6929ae7f6f23b1e52d84398d0b363
Johannes Zellner <johannes@cloudron.io>
2017-11-17 23:45:22 +0100

    Also fix the restoreConfigJson migration down script

--- migrations/20171116224051-apps-rename-lastBackupId-to-restoreConfigJson.js
+++ migrations/20171116224051-apps-rename-lastBackupId-to-restoreConfigJson.js
@@ -8,7 +8,7 @@ exports.up = function(db, callback) {
 };
 
 exports.down = function(db, callback) {
-    db.runSql('ALTER TABLE apps CHANGE lastBackupId restoreConfigJson VARCHAR(256)', [], function (error) {
+    db.runSql('ALTER TABLE apps CHANGE restoreConfigJson lastBackupId VARCHAR(256)', [], function (error) {
         if (error) console.error(error);
         callback(error);
     });
