8c8e3870125a81a8a9e3a88bfef882fd5aeb9957
Girish Ramakrishnan <girish@cloudron.io>
2018-01-17 20:25:57 -0800

    bump addons

--- src/infra_version.js
+++ src/infra_version.js
@@ -14,11 +14,11 @@ exports = module.exports = {
     // Note that if any of the databases include an upgrade, bump the infra version above
     // This is because we upgrade using dumps instead of mysql_upgrade, pg_upgrade etc
     'images': {
-        'mysql': { repo: 'cloudron/mysql', tag: 'cloudron/mysql:0.18.0' },
-        'postgresql': { repo: 'cloudron/postgresql', tag: 'cloudron/postgresql:0.17.1' },
-        'mongodb': { repo: 'cloudron/mongodb', tag: 'cloudron/mongodb:0.13.0' },
-        'redis': { repo: 'cloudron/redis', tag: 'cloudron/redis:0.11.0' },
+        'mysql': { repo: 'cloudron/mysql', tag: 'cloudron/mysql:1.0.0' },
+        'postgresql': { repo: 'cloudron/postgresql', tag: 'cloudron/postgresql:1.0.0' },
+        'mongodb': { repo: 'cloudron/mongodb', tag: 'cloudron/mongodb:1.0.1' },
+        'redis': { repo: 'cloudron/redis', tag: 'cloudron/redis:1.0.0' },
         'mail': { repo: 'cloudron/mail', tag: 'cloudron/mail:0.40.0' },
-        'graphite': { repo: 'cloudron/graphite', tag: 'cloudron/graphite:0.12.0' }
+        'graphite': { repo: 'cloudron/graphite', tag: 'cloudron/graphite:1.0.0' }
     }
 };
