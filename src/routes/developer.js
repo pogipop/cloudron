497c76a9058cc736861c018136d8dd2faf6e6fbd
Johannes Zellner <johannes@cloudron.io>
2018-04-26 20:10:46 +0200

    Add 2fa token check to developer login

--- src/routes/developer.js
+++ src/routes/developer.js
@@ -7,7 +7,8 @@ exports = module.exports = {
 var developer = require('../developer.js'),
     passport = require('passport'),
     HttpError = require('connect-lastmile').HttpError,
-    HttpSuccess = require('connect-lastmile').HttpSuccess;
+    HttpSuccess = require('connect-lastmile').HttpSuccess,
+    speakeasy = require('speakeasy');
 
 function login(req, res, next) {
     passport.authenticate('local', function (error, user) {
@@ -15,6 +16,13 @@ function login(req, res, next) {
         if (!user) return next(new HttpError(401, 'Invalid credentials'));
 
         var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || null;
+
+        if (user.twoFactorAuthenticationEnabled) {
+            if (!req.body.totpToken) return next(new HttpError(401, 'A totpToken must be provided'));
+
+            let verified = speakeasy.totp.verify({ secret: user.twoFactorAuthenticationSecret, encoding: 'base32', token: req.body.totpToken });
+            if (!verified) return next(new HttpError(401, 'Invalid totpToken'));
+        }
 
         developer.issueDeveloperToken(user, ip, function (error, result) {
             if (error) return next(new HttpError(500, error));
