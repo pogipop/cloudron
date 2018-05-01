97acd408296ef71f9ed7258a5e670c0e873f12bb
Girish Ramakrishnan <girish@cloudron.io>
2018-05-01 14:51:23 -0700

    remove obsolete roleSdk from the database
    the file is renamed because cloudron.io was patched by mistake and we want to run this
    migration there

---
+++ migrations/20180429235008-fixes-scopes.js
@@ -0,0 +1,17 @@
+'use strict';
+
+exports.up = function(db, callback) {
+    db.runSql('UPDATE clients SET scope=? WHERE id=? OR id=? OR id=?', ['*', 'cid-webadmin', 'cid-sdk', 'cid-cli'], function (error) {
+        if (error) console.error(error);
+
+        db.runSql('UPDATE tokens SET scope=? WHERE scope LIKE ?', ['*', '%*%'], function (error) {
+            if (error) console.error(error);
+
+            callback(error);
+        });
+    });
+};
+
+exports.down = function(db, callback) {
+    callback();
+};
