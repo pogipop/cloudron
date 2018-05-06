31af6c64d05112ca6795f6a61182f8ffbe97e150
Girish Ramakrishnan <girish@cloudron.io>
2018-05-06 13:08:22 -0700

    Expire existing webadmin token so that the UI gets a new token

--- migrations/20180429235008-fixes-scopes.js
+++ migrations/20180429235008-fixes-scopes.js
@@ -4,10 +4,14 @@ exports.up = function(db, callback) {
     db.runSql('UPDATE clients SET scope=? WHERE id=? OR id=? OR id=?', ['*', 'cid-webadmin', 'cid-sdk', 'cid-cli'], function (error) {
         if (error) console.error(error);
 
-        db.runSql('UPDATE tokens SET scope=? WHERE scope LIKE ?', ['*', '%*%'], function (error) {
+        db.runSql('UPDATE tokens SET scope=? WHERE scope LIKE ?', ['*', '%*%'], function (error) { // remove the roleSdk
             if (error) console.error(error);
 
-            callback(error);
+            db.runSql('UPDATE tokens SET expires=? WHERE clientId=?', [ 1525636734905, 'cid-webadmin' ], function (error) { // force webadmin to get a new token
+                if (error) console.error(error);
+
+                callback(error);
+            });
         });
     });
 };
