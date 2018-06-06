898d928dd67947eb811dceaf597d1866f8974f9b
Johannes Zellner <johannes@cloudron.io>
2018-06-06 18:45:54 +0200

    logrotate files under platformdata must be owned by root

--- setup/start.sh
+++ setup/start.sh
@@ -237,9 +237,12 @@ fi
 
 echo "==> Changing ownership"
 chown "${USER}:${USER}" -R "${CONFIG_DIR}"
-chown "${USER}:${USER}" -R "${PLATFORM_DATA_DIR}/nginx" "${PLATFORM_DATA_DIR}/collectd" "${PLATFORM_DATA_DIR}/logrotate.d" "${PLATFORM_DATA_DIR}/addons" "${PLATFORM_DATA_DIR}/acme" "${PLATFORM_DATA_DIR}/backup"
+chown "${USER}:${USER}" -R "${PLATFORM_DATA_DIR}/nginx" "${PLATFORM_DATA_DIR}/collectd" "${PLATFORM_DATA_DIR}/addons" "${PLATFORM_DATA_DIR}/acme" "${PLATFORM_DATA_DIR}/backup"
 chown "${USER}:${USER}" "${PLATFORM_DATA_DIR}/INFRA_VERSION" 2>/dev/null || true
 chown "${USER}:${USER}" "${PLATFORM_DATA_DIR}"
+
+# logrotate files have to be owned by root, this is here to fixup existing installations where we were resetting the owner to yellowtent
+chown root:root -R "${PLATFORM_DATA_DIR}/logrotate.d"
 
 # do not chown the boxdata/mail directory; dovecot gets upset
 chown "${USER}:${USER}" "${BOX_DATA_DIR}"
