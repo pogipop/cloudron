0b82146b3ee1704c0f5f9030a0bd5672387d4d4d
Girish Ramakrishnan <girish@cloudron.io>
2018-06-11 22:13:27 -0700

    Install cloudron-syslog service file from box repo

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -94,12 +94,13 @@ if [[ ${try} -eq 10 ]]; then
 fi
 
 echo "==> installer: update cloudron-syslog"
-if [[ "$(cloudron-syslog --version)" != "1.0.0" ]]; then
-    CLOUDRON_SYSLOG_DIR=/usr/local/cloudron-syslog
-    rm -rf ${CLOUDRON_SYSLOG_DIR}
-    mkdir -p ${CLOUDRON_SYSLOG_DIR}
-    $curl -sL https://git.cloudron.io/cloudron/cloudron-syslog/-/archive/v1.0.0/cloudron-syslog-v1.0.0.tar.gz | tar zxvf - --strip-components=1 -C ${CLOUDRON_SYSLOG_DIR}
-    (cd ${CLOUDRON_SYSLOG_DIR} && npm install --unsafe-perm) # need --unsafe-perm since we run as root and npm drops privileges
+CLOUDRON_SYSLOG_DIR=/usr/local/cloudron-syslog
+if [[ "$($CLOUDRON_SYSLOG_DIR/bin/cloudron-syslog --version)" != "1.0.0" ]]; then
+    rm -rf "${CLOUDRON_SYSLOG_DIR}"
+    mkdir -p "${CLOUDRON_SYSLOG_DIR}"
+    if npm install --unsafe-perm -g --prefix "${CLOUDRON_SYSLOG_DIR}" cloudron-syslog; then break; fi
+    echo "===> installer: Failed to install cloudron-syslog, trying again"
+    sleep 5
 fi
 
 if ! id "${USER}" 2>/dev/null; then
