3601e4f8a6100c4146b8b94f77322a4e2657452b
Girish Ramakrishnan <girish@cloudron.io>
2018-06-18 17:39:15 -0700

    if -> while

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -95,7 +95,7 @@ fi
 
 echo "==> installer: update cloudron-syslog"
 CLOUDRON_SYSLOG_DIR=/usr/local/cloudron-syslog
-if [[ "$($CLOUDRON_SYSLOG_DIR/bin/cloudron-syslog --version)" != "1.0.2" ]]; then
+while [[ "$($CLOUDRON_SYSLOG_DIR/bin/cloudron-syslog --version)" != "1.0.2" ]]; then
     rm -rf "${CLOUDRON_SYSLOG_DIR}"
     mkdir -p "${CLOUDRON_SYSLOG_DIR}"
     if npm install --unsafe-perm -g --prefix "${CLOUDRON_SYSLOG_DIR}" cloudron-syslog@1.0.2; then break; fi
