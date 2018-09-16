e2b388f721f205896dfc65d542ae3196b4c2f612
Girish Ramakrishnan <girish@cloudron.io>
2018-09-15 20:14:16 -0700

    Update redis addon that has clear route

--- src/infra_version.js
+++ src/infra_version.js
@@ -18,7 +18,7 @@ exports = module.exports = {
         'mysql': { repo: 'cloudron/mysql', tag: 'cloudron/mysql:1.1.0@sha256:0459023f16e65985e8d74b490b5c4f38c9d1b7a4e5ec8049c08256d42decf00e' },
         'postgresql': { repo: 'cloudron/postgresql', tag: 'cloudron/postgresql:1.1.0@sha256:731d802211fa08ab951ebac2565048d44526c73948245f18df0ffc27929a8a08' },
         'mongodb': { repo: 'cloudron/mongodb', tag: 'cloudron/mongodb:2.0.0@sha256:b1d8877755463cfc2c14e43d750b9e9d391e1a68814ac0d3ba0f8963e667480a' },
-        'redis': { repo: 'cloudron/redis', tag: 'cloudron/redis:2.0.0@sha256:3c35702a751e2b8dfa4f7cdf41c7f899f62329a09beb09b5292b2c1bfe1f6ccb' },
+        'redis': { repo: 'cloudron/redis', tag: 'cloudron/redis:2.0.1@sha256:73d300c87d0fd1b8f01157fd69c65aa04377498bc7daa7aafd6d6fdcf2e5a35e' },
         'mail': { repo: 'cloudron/mail', tag: 'cloudron/mail:1.4.0@sha256:28e65b446569a324f4b28e920d43ac9723f9aa9699a629bec7368a2a74669f88' },
         'graphite': { repo: 'cloudron/graphite', tag: 'cloudron/graphite:1.0.0@sha256:196bf1d19782a015b361da48d06ba1017b3d04b4256d93fbb9c0b50599f69f5d' }
     }
