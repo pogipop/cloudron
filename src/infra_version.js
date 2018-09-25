1fed7335cf8ac2af45c00ae4893c1c54807db09c
Girish Ramakrishnan <girish@cloudron.io>
2018-09-25 14:24:32 -0700

    Update the db addons

--- src/infra_version.js
+++ src/infra_version.js
@@ -15,9 +15,9 @@ exports = module.exports = {
     // This is because we upgrade using dumps instead of mysql_upgrade, pg_upgrade etc
     // docker inspect --format='{{index .RepoDigests 0}}' $IMAGE to get the sha256
     'images': {
-        'mysql': { repo: 'cloudron/mysql', tag: 'cloudron/mysql:2.0.1@sha256:42c861338d93beadc11438031ad7f68a10b1582fc27c8432173910d6b4f0e15d' },
-        'postgresql': { repo: 'cloudron/postgresql', tag: 'cloudron/postgresql:2.0.1@sha256:a40096896960594411864aac1890e2a883639d6df810c2ae7b720ffa52be3681' },
-        'mongodb': { repo: 'cloudron/mongodb', tag: 'cloudron/mongodb:2.0.1@sha256:b320fdea0d256fca73de40370c1029e2624c52620ec178716f59db152aef2d15' },
+        'mysql': { repo: 'cloudron/mysql', tag: 'cloudron/mysql:2.0.1@sha256:f6a0b051cff3593fd9e4bf55158d7107fb2a3f384a9a9a192ed65a58ed5ee6b7' },
+        'postgresql': { repo: 'cloudron/postgresql', tag: 'cloudron/postgresql:2.0.1@sha256:520528f9fc2b11297d478c1b0c6640ec40c92602ee308097efa856cf8b9472a7' },
+        'mongodb': { repo: 'cloudron/mongodb', tag: 'cloudron/mongodb:2.0.1@sha256:18bf7bc0032a5867a34b507532744adddc3b1c5e8c0bddca2b9ae5d33bebdecb' },
         'redis': { repo: 'cloudron/redis', tag: 'cloudron/redis:2.0.1@sha256:ac936cecbbdda8dc25d3ff08e958282190b96538628f10ad5bb55ae513a043b1' },
         'mail': { repo: 'cloudron/mail', tag: 'cloudron/mail:1.4.0@sha256:28e65b446569a324f4b28e920d43ac9723f9aa9699a629bec7368a2a74669f88' },
         'graphite': { repo: 'cloudron/graphite', tag: 'cloudron/graphite:1.0.0@sha256:196bf1d19782a015b361da48d06ba1017b3d04b4256d93fbb9c0b50599f69f5d' }
