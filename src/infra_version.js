a6618c5813d295b3c6e314bd88866e0e5cd3a1e6
Girish Ramakrishnan <girish@cloudron.io>
2018-10-15 23:45:54 -0700

    Update mongodb (healthcheck)

--- src/infra_version.js
+++ src/infra_version.js
@@ -9,7 +9,7 @@ exports = module.exports = {
     // a minor version makes all apps re-configure themselves
     'version': '48.12.0',
 
-    'baseImages': [ 'cloudron/base:1.0.0@sha256:147a648a068a2e746644746bbfb42eb7a50d682437cead3c67c933c546357617' ],
+    'baseImages': [ 'cloudron/base:1.0.0@sha256:910112e46cb066505cf49b86efee68858714cabe8f55c605f30eeb83fb894760' ],
 
     // Note that if any of the databases include an upgrade, bump the infra version above
     // This is because we upgrade using dumps instead of mysql_upgrade, pg_upgrade etc
@@ -17,7 +17,7 @@ exports = module.exports = {
     'images': {
         'mysql': { repo: 'cloudron/mysql', tag: 'cloudron/mysql:2.0.0@sha256:1c177c3fa079695aea13cec6daf52b772f400022131f31e8da237f55d683d9f4' },
         'postgresql': { repo: 'cloudron/postgresql', tag: 'cloudron/postgresql:2.0.0@sha256:ef357c0319e50fcc64cc5fa19e31e73b0632e48073d44024399fe93fbe8aaf82' },
-        'mongodb': { repo: 'cloudron/mongodb', tag: 'cloudron/mongodb:2.0.0@sha256:ef77e926811b775dd2b208ec619c902c22cb583838663cdd901811dacfbedbb9' },
+        'mongodb': { repo: 'cloudron/mongodb', tag: 'cloudron/mongodb:2.0.0@sha256:4cefe45335c12e89b52f20c2817f848f52e6025011adf9422ab9a500f205f34b' },
         'redis': { repo: 'cloudron/redis', tag: 'cloudron/redis:2.0.0@sha256:8a88dd334b62b578530a014ca1a2425a54cb9df1e475f5d3a36806e5cfa22121' },
         'mail': { repo: 'cloudron/mail', tag: 'cloudron/mail:2.0.0@sha256:eb2463d768cccb681c404eae6d06516688dc04d8b64cbc49b3b834b78cef0a61' },
         'graphite': { repo: 'cloudron/graphite', tag: 'cloudron/graphite:2.0.0@sha256:454f035d60b768153d4f31210380271b5ba1c09367c9d95c7fa37f9e39d2f59c' }
