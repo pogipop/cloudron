cf8bb3da9eb154cd5bb260956915675fc4054de7
Girish Ramakrishnan <girish@cloudron.io>
2018-10-17 18:40:48 -0700

    Update mysql (prefix backup and restore)

--- src/infra_version.js
+++ src/infra_version.js
@@ -13,7 +13,7 @@ exports = module.exports = {
     // a major version bump in the db containers will trigger the restore logic that uses the db dumps
     // docker inspect --format='{{index .RepoDigests 0}}' $IMAGE to get the sha256
     'images': {
-        'mysql': { repo: 'cloudron/mysql', tag: 'cloudron/mysql:2.0.0@sha256:1c177c3fa079695aea13cec6daf52b772f400022131f31e8da237f55d683d9f4' },
+        'mysql': { repo: 'cloudron/mysql', tag: 'cloudron/mysql:2.0.0@sha256:56253ab5f98e9d3cfd9b0aa65836c2b04e3f42f480b9dcd82f2f79cb762c0fad' },
         'postgresql': { repo: 'cloudron/postgresql', tag: 'cloudron/postgresql:2.0.0@sha256:ef357c0319e50fcc64cc5fa19e31e73b0632e48073d44024399fe93fbe8aaf82' },
         'mongodb': { repo: 'cloudron/mongodb', tag: 'cloudron/mongodb:2.0.0@sha256:910112e46cb066505cf49b86efee68858714cabe8f55c605f30eeb83fb894760' },
         'redis': { repo: 'cloudron/redis', tag: 'cloudron/redis:2.0.0@sha256:8a88dd334b62b578530a014ca1a2425a54cb9df1e475f5d3a36806e5cfa22121' },
