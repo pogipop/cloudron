28d61a4d70334e75da25f518971a40566a889d2d
Johannes Zellner <johannes@cloudron.io>
2018-10-23 13:05:39 +0200

    Use postgres addon with fixed restore

--- src/infra_version.js
+++ src/infra_version.js
@@ -14,7 +14,7 @@ exports = module.exports = {
     // docker inspect --format='{{index .RepoDigests 0}}' $IMAGE to get the sha256
     'images': {
         'mysql': { repo: 'cloudron/mysql', tag: 'cloudron/mysql:2.0.0@sha256:56253ab5f98e9d3cfd9b0aa65836c2b04e3f42f480b9dcd82f2f79cb762c0fad' },
-        'postgresql': { repo: 'cloudron/postgresql', tag: 'cloudron/postgresql:2.0.0@sha256:95a6ecc2d320118ef41cd62c87b9ce0d95af91295fb0b5b003e3e02059d435b5' },
+        'postgresql': { repo: 'cloudron/postgresql', tag: 'cloudron/postgresql:2.0.1@sha256:3cb04e1ca7fb3ac402eb0603b4cbaec8bacc531b40f903655ec7b979c757a8d7' },
         'mongodb': { repo: 'cloudron/mongodb', tag: 'cloudron/mongodb:2.0.0@sha256:910112e46cb066505cf49b86efee68858714cabe8f55c605f30eeb83fb894760' },
         'redis': { repo: 'cloudron/redis', tag: 'cloudron/redis:2.0.0@sha256:8a88dd334b62b578530a014ca1a2425a54cb9df1e475f5d3a36806e5cfa22121' },
         'mail': { repo: 'cloudron/mail', tag: 'cloudron/mail:2.0.0@sha256:3c0fbb2a042ac471940ac3e9f6ffa900c8a294941fb7de509b2e3309b09fbffd' },
