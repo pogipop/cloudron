1bb255238466ea751d43a7e5cf4b07f2f6b765d6
Girish Ramakrishnan <girish@cloudron.io>
2018-12-19 14:30:04 -0800

    move feedback test

--- src/routes/test/support-test.js
+++ src/routes/test/support-test.js
@@ -9,8 +9,11 @@ var async = require('async'),
     config = require('../../config.js'),
     database = require('../../database.js'),
     expect = require('expect.js'),
+    nock = require('nock'),
     path = require('path'),
     safe = require('safetydance'),
+    settings = require('../../settings.js'),
+    settingsdb = require('../../settingsdb.js'),
     superagent = require('superagent'),
     server = require('../../server.js');
 
@@ -21,6 +24,7 @@ var AUTHORIZED_KEYS_FILE = path.join(config.baseDir(), 'authorized_keys');
 var token = null;
 
 function setup(done) {
+    nock.cleanAll();
     config._reset();
     config.setFqdn('example-ssh-test.com');
     safe.fs.unlinkSync(AUTHORIZED_KEYS_FILE);
@@ -43,23 +47,27 @@ function setup(done) {
 
                     callback();
                 });
-        }
+        },
+
+        settingsdb.set.bind(null, settings.APPSTORE_CONFIG_KEY, JSON.stringify({ userId: 'USER_ID', cloudronId: 'CLOUDRON_ID', token: 'ACCESS_TOKEN' }))
     ], done);
 }
 
 function cleanup(done) {
     database._clear(function (error) {
-        expect(!error).to.be.ok();
+        expect(error).to.not.be.ok();
+
+        config._reset();
 
         server.stop(done);
     });
 }
 
 describe('Support API', function () {
-    before(setup);
-    after(cleanup);
-
     describe('remote support', function () {
+        before(setup);
+        after(cleanup);
+
         it('get remote support', function (done) {
             superagent.get(SERVER_URL + '/api/v1/support/remote_support')
                 .query({ access_token: token })
@@ -132,6 +140,122 @@ describe('Support API', function () {
                     let data = safe.fs.readFileSync(AUTHORIZED_KEYS_FILE, 'utf8');
                     let count = (data.match(/support@cloudron.io/g) || []).length;
                     expect(count).to.be(0);
+                    done();
+                });
+        });
+    });
+
+    describe('feedback', function () {
+        before(setup);
+        after(cleanup);
+
+        it('fails without token', function (done) {
+            superagent.post(SERVER_URL + '/api/v1/support/feedback')
+                .send({ type: 'ticket', subject: 'some subject', description: 'some description' })
+                .end(function (error, result) {
+                    expect(result.statusCode).to.equal(401);
+                    done();
+                });
+        });
+
+        it('fails without type', function (done) {
+            superagent.post(SERVER_URL + '/api/v1/support/feedback')
+                .send({ subject: 'some subject', description: 'some description' })
+                .query({ access_token: token })
+                .end(function (error, result) {
+                    expect(result.statusCode).to.equal(400);
+                    done();
+                });
+        });
+
+        it('fails with empty type', function (done) {
+            superagent.post(SERVER_URL + '/api/v1/support/feedback')
+                .send({ type: '', subject: 'some subject', description: 'some description' })
+                .query({ access_token: token })
+                .end(function (error, result) {
+                    expect(result.statusCode).to.equal(400);
+                    done();
+                });
+        });
+
+        it('fails with unknown type', function (done) {
+            superagent.post(SERVER_URL + '/api/v1/support/feedback')
+                .send({ type: 'foobar', subject: 'some subject', description: 'some description' })
+                .query({ access_token: token })
+                .end(function (error, result) {
+                    expect(result.statusCode).to.equal(400);
+                    done();
+                });
+        });
+
+        it('fails without description', function (done) {
+            superagent.post(SERVER_URL + '/api/v1/support/feedback')
+                .send({ type: 'ticket', subject: 'some subject' })
+                .query({ access_token: token })
+                .end(function (error, result) {
+                    expect(result.statusCode).to.equal(400);
+                    done();
+                });
+        });
+
+        it('fails with empty subject', function (done) {
+            superagent.post(SERVER_URL + '/api/v1/support/feedback')
+                .send({ type: 'ticket', subject: '', description: 'some description' })
+                .query({ access_token: token })
+                .end(function (error, result) {
+                    expect(result.statusCode).to.equal(400);
+                    done();
+                });
+        });
+
+        it('fails with empty description', function (done) {
+            superagent.post(SERVER_URL + '/api/v1/support/feedback')
+                .send({ type: 'ticket', subject: 'some subject', description: '' })
+                .query({ access_token: token })
+                .end(function (error, result) {
+                    expect(result.statusCode).to.equal(400);
+                    done();
+                });
+        });
+
+        it('fails without subject', function (done) {
+            superagent.post(SERVER_URL + '/api/v1/support/feedback')
+                .send({ type: 'ticket', description: 'some description' })
+                .query({ access_token: token })
+                .end(function (error, result) {
+                    expect(result.statusCode).to.equal(400);
+                    done();
+                });
+        });
+
+        it('succeeds with ticket type', function (done) {
+            var scope2 = nock(config.apiServerOrigin())
+                .filteringRequestBody(function (/* unusedBody */) { return ''; }) // strip out body
+                .post('/api/v1/users/USER_ID/cloudrons/CLOUDRON_ID/feedback?accessToken=ACCESS_TOKEN')
+                .reply(201, { });
+
+            superagent.post(SERVER_URL + '/api/v1/support/feedback')
+                .send({ type: 'ticket', subject: 'some subject', description: 'some description' })
+                .query({ access_token: token })
+                .end(function (error, result) {
+                    expect(result.statusCode).to.equal(201);
+                    expect(scope2.isDone()).to.be.ok();
+                    done();
+                });
+        });
+
+        it('succeeds with app type', function (done) {
+            var scope2 = nock(config.apiServerOrigin())
+                .filteringRequestBody(function (/* unusedBody */) { return ''; }) // strip out body
+                .post('/api/v1/users/USER_ID/cloudrons/CLOUDRON_ID/feedback?accessToken=ACCESS_TOKEN')
+                .reply(201, { });
+
+            superagent.post(SERVER_URL + '/api/v1/support/feedback')
+                .send({ type: 'app_missing', subject: 'some subject', description: 'some description' })
+                .query({ access_token: token })
+                .end(function (error, result) {
+                    expect(result.statusCode).to.equal(201);
+                    expect(scope2.isDone()).to.be.ok();
                     done();
                 });
         });
