b771df88da567dd11ff3e59185563bd61975cab0
Johannes Zellner <johannes@cloudron.io>
2019-01-19 15:41:47 +0100

    Ensure we write process crash logs to disk

--- src/logcollector.js
+++ src/logcollector.js
@@ -15,7 +15,6 @@ var COLLECT_LOGS_CMD = path.join(__dirname, 'scripts/collectlogs.sh');
 var CRASH_LOG_TIMESTAMP_OFFSET = 1000 * 60 * 60; // 60 min
 var CRASH_LOG_TIMESTAMP_FILE = '/tmp/crashlog.timestamp';
 var CRASH_LOG_STASH_FILE = '/tmp/crashlog';
-var CRASH_LOG_FILE_LIMIT = 2 * 1024 * 1024; // 2mb
 
 function collectLogs(unitName, callback) {
     assert.strictEqual(typeof unitName, 'string');
@@ -29,40 +28,29 @@ function collectLogs(unitName, callback) {
     callback(null, logs);
 }
 
-function stashLogs(logs) {
-    var stat = safe.fs.statSync(CRASH_LOG_STASH_FILE);
-    if (stat && (stat.size > CRASH_LOG_FILE_LIMIT)) {
-        console.error('Dropping logs since crash file has become too big');
-        return;
-    }
-
-    safe.fs.writeFileSync(CRASH_LOG_STASH_FILE, logs);
-}
-
 function sendFailureLogs(unitName) {
     assert.strictEqual(typeof unitName, 'string');
 
-    collectLogs(unitName, function (error, newLogs) {
+    collectLogs(unitName, function (error, logs) {
         if (error) {
             console.error('Failed to collect logs.', error);
-            newLogs = util.format('Failed to collect logs.', error);
+            logs = util.format('Failed to collect logs.', error);
         }
 
         console.log('Sending failure logs for', unitName);
+
+        if (!safe.fs.writeFileSync(CRASH_LOG_STASH_FILE, logs)) console.log(`Failed to stash logs to ${CRASH_LOG_STASH_FILE}`);
 
         var timestamp = safe.fs.readFileSync(CRASH_LOG_TIMESTAMP_FILE, 'utf8');
 
         // check if we already sent a mail in the last CRASH_LOG_TIME_OFFSET window
         if (timestamp && (parseInt(timestamp) + CRASH_LOG_TIMESTAMP_OFFSET) > Date.now()) {
             console.log('Crash log already sent within window. Stashing logs.');
-            return stashLogs(newLogs);
+            return;
         }
 
         eventlog.add(eventlog.ACTION_PROCESS_CRASH, { processName: unitName }, { crashLogFile: CRASH_LOG_STASH_FILE }, function (error) {
-            if (error) {
-                console.log(`Error sending crashlog. Logs stashed at ${CRASH_LOG_STASH_FILE}`);
-                return stashLogs(newLogs);
-            }
+            if (error) console.log(`Error sending crashlog. Logs stashed at ${CRASH_LOG_STASH_FILE}`);
 
             // write the new timestamp file and delete stash file
             safe.fs.writeFileSync(CRASH_LOG_TIMESTAMP_FILE, String(Date.now()));
